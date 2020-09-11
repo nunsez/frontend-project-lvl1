07800d8a636dc88f0c18ed208c354f21dc222a89
Alexander Mandrikov <mad.nunsez@gmail.com>
2020-09-11 21:18:36 +0700

    refactor brain calc source file

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,33 +1,30 @@
 import _ from 'lodash';
 
-// * operations = ['+', '-', '*'];
 const prefix = 'What is the result of the expression?';
 
+const operations = ['+', '-', '*'];
 const firstNumber = [];
 const secondNumber = [];
-const currentOperationIndex = [];
+const operationIndex = [];
 const task = [];
 const rightAnswer = [];
 
 for (let i = 0; i < 3; i += 1) {
   firstNumber[i] = _.random(1, 49);
   secondNumber[i] = _.random(1, 49);
-  currentOperationIndex[i] = _.random(0, 2);
+  operationIndex[i] = _.random(0, 2);
+  task[i] = `${firstNumber[i]} ${operations[operationIndex[i]]} ${
+    secondNumber[i]
+  }`;
 
-  switch (currentOperationIndex[i]) {
+  switch (operationIndex[i]) {
     case 0:
-      task[i] = `${firstNumber[i]} + ${secondNumber[i]}`;
       rightAnswer[i] = firstNumber[i] + secondNumber[i];
       break;
     case 1:
-      task[i] =
-        firstNumber[i] > secondNumber[i]
-          ? `${firstNumber[i]} - ${secondNumber[i]}`
-          : `${secondNumber[i]} - ${firstNumber[i]}`;
-      rightAnswer[i] = Math.abs(firstNumber[i] - secondNumber[i]);
+      rightAnswer[i] = firstNumber[i] - secondNumber[i];
       break;
     case 2:
-      task[i] = `${firstNumber[i]} * ${secondNumber[i]}`;
       rightAnswer[i] = firstNumber[i] * secondNumber[i];
       break;
     default:
